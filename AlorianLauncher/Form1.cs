a18e66b2f544d692dddc4fdec71855de2f177c37
Efendy <134609849+efxndyben@users.noreply.github.com>
2023-06-04 16:02:03 +0300

    Update Form1.cs

--- AlorianLauncher/Form1.cs
+++ AlorianLauncher/Form1.cs
@@ -1,3 +1,8 @@
+using DiscordRPC;
+using DiscordRPC.Logging;
+using DiscordRPC.Events;
+
+
 namespace AlorianLauncher
 {
     public partial class Form1 : Form
@@ -5,6 +10,35 @@ namespace AlorianLauncher
         public Form1()
         {
             InitializeComponent();
+            Initialize();
+        }
+
+        public DiscordRpcClient client { get; private set; }
+
+        //Called when your application first starts.
+        //For example, just before your main loop, on OnEnable for unity.
+        void Initialize()
+        {
+            client = new DiscordRpcClient("1114618806275416198");
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "play.alorianmc.net",
+                State = "Başlatılıyor",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "alrcraft",
+                    LargeImageText = "AlorianMC",
+                    SmallImageKey = "alrcraft_transparent",
+                    SmallImageText = "play.alorianmc.net"
+                }
+            });
+        }
+
+        void Deinitialize()
+        {
+            client.Dispose();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -26,6 +60,7 @@ namespace AlorianLauncher
                 LoginPage lg = new LoginPage();
                 lg.Show();
                 this.Hide();
+                Deinitialize();
 
             }
         }
